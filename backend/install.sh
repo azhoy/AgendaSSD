eb4db9ba521dd10f335d2de1f4456721006d31a5
Ayoub Touhami <ayoub.touhami@ulb.be>
2023-01-08 23:49:19 +0000

    Update 3 files
    - /backend/requirements.txt
    - /backend/install.sh
    - /backend/ReportAgendaSSD-1.pdf

---
+++ backend/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Run as super user
+
+#== Ubuntu/Debian ==#
+apt update -y
+apt install software-properties-common
+add-apt-repository --yes --update ppa:deadsnakes/ppa
+apt update -y
+apt install pip -y
+apt install npm -y
+
+
+# Install
+cd backend
+pip install -r /backend/requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+
+# Install
+cd ../../frontend
+sudo apt install -y npm
+npm run dev
+
+
