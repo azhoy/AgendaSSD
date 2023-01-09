83e2760b35d47eeb4de9308ed9e7f589609b1f0a
Ayoub Touhami <ayoub.touhami@ulb.be>
2023-01-09 00:20:29 +0000

    Update 2 files
    - /install.sh
    - /backend/agenda/settings.py

--- backend/agenda/settings.py
+++ backend/agenda/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5173',
+    'http://localhost:5173',
 ]
 
 MIDDLEWARE = [
--- install.sh
+++ install.sh
@@ -9,11 +9,12 @@ add-apt-repository --yes --update ppa:deadsnakes/ppa
 apt update -y
 apt install pip -y
 apt install npm -y
+apt install node -y
 
 
 # Install
 cd backend
-pip install -r requirements.txt
+pip install -r ./backend/requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
