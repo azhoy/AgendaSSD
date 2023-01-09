6aab4c41c0aa693e45f16af751004c3999eabe22
Ayoub Touhami <ayoub.touhami@ulb.be>
2023-01-09 00:28:17 +0000

    Update file install.sh

--- install.sh
+++ install.sh
@@ -8,20 +8,13 @@ apt install software-properties-common
 add-apt-repository --yes --update ppa:deadsnakes/ppa
 apt update -y
 apt install pip -y
-apt install npm -y
 apt install node -y
 
 
-# Install
-cd backend
-pip install -r ./backend/requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
+# Install 
+pip install -r /backend/requirements.txt
 
 # Install
-cd ../../frontend
+
 sudo apt install -y npm
-npm run dev
-
-
+sudo npm install --prefix ./frontend
